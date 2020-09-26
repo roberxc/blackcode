d0610b81d1a217737ac34e724db61ad04d340ee1
unknown <ddeltax2@gmail.com>
2020-09-25 22:51:26 -0300

    Vista de login como ejemplo

--- application/views/Login.php
+++ application/views/Login.php
@@ -21,12 +21,12 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>CDH</b>Ingenieria</a>
+    <a href="../../index2.html"><b>CDH</b> Ingenieria</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">Ingresa tus datos</p>
 
       <form action="<?php echo base_url()?>Inicio" method="post">
         <div class="input-group mb-3">
@@ -38,7 +38,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Contraseña">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -47,21 +47,25 @@
         </div>
         <div class="row">
           <div class="col-8">
+          <!--
             <div class="icheck-primary">
               <input type="checkbox" id="remember">
               <label for="remember">
                 Remember Me
               </label>
             </div>
+            -->
           </div>
+          
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
 
+      <!--
       <div class="social-auth-links text-center mb-3">
         <p>- OR -</p>
         <a href="#" class="btn btn-block btn-primary">
@@ -71,7 +75,8 @@
           <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
         </a>
       </div>
-      <!-- /.social-auth-links -->
+      
+
 
       <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
@@ -79,6 +84,7 @@
       <p class="mb-0">
         <a href="register.html" class="text-center">Register a new membership</a>
       </p>
+      -->
     </div>
     <!-- /.login-card-body -->
   </div>
