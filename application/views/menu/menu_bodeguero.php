3746f2affd198c4d24aea6a24bf7d6473fa4953d
unknown <ossandonrojasdaniel@gmail.com>
2020-11-09 23:59:16 -0300

    avance previo

--- application/views/menu/menu_bodeguero.php
+++ application/views/menu/menu_bodeguero.php
@@ -8,9 +8,9 @@ if (isset($set_data['nombre_completo'])) {
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?php echo base_url()?>Inicio" class="brand-link">
-      <img src="<?php echo base_url();?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?php echo base_url();?>assets/dist/img/black.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">BlackCodef</span>
+      <span class="brand-text font-weight-light">BlackCode</span>
     </a>
 
     <!-- Sidebar -->
@@ -18,7 +18,7 @@ if (isset($set_data['nombre_completo'])) {
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?php echo base_url();?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php echo base_url();?>assets/dist/img/0012.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo $nombre;?></a>
