6a72659d6f2229316921173ea70b3936af9ea931
unknown <dioskratos52@gmail.com>
2021-01-14 20:08:17 -0300

    proyectov8

--- application/views/Proyecto/Documentos.php
+++ application/views/Proyecto/Documentos.php
@@ -8,7 +8,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Ordenes de compraa</h1>
+            <h1>Ordenes de compra</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
