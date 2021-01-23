9a7bf4482c95f1121a57938bdae97878fd8db2ec
unknown <ossandonrojasdaniel@gmail.com>
2021-01-23 00:54:16 -0300

    tiodani

--- application/views/Administracion/VGarage.php
+++ application/views/Administracion/VGarage.php
@@ -6,14 +6,6 @@
    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
    <!-- DataTables -->
-   <link rel="styl<head>
-   <!-- SELECT CON BUSCADOR -->
-   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
-   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" rel="stylesheet" />
-   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.css" rel="stylesheet" />
-   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-   <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-   <!-- DataTables -->
    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.jqueryui.min.css">
 </head>
@@ -30,7 +22,7 @@
          <!-- /.row -->
       </div>
       <!-- /.container-fluid -->
-
+      <!-- /.container-fluid -->
    </div>
    <section class="content">
       <div class="box box-info ">
@@ -39,7 +31,7 @@
                <table id="example1" name="example1" class="table table-bordered table-striped" style="width: 100%;">
                   <thead>
                      <tr>
-                        <th>Codigo</th>
+                     <th>Codigo</th>
                         <!-- 0 ---> 
                         <th>Patente</th>
                         <!-- 1 --->
@@ -57,81 +49,6 @@
                         <!-- 7 --->
                         <th>Gps</th>
                         <!-- 8 --->
-
-                        
-                     
-                     </tr>
-                  </thead>
-               </table>
-            </div>
-         </div>
-      </div>
-</div>
-</div>
-</section>     
-</body>  
-</html> 
-<div id="modal"></div>
-
-
-<!--Mostrar tabla principal-->
-<script> 
-   $(document).ready(function(){
-     $('#example1').DataTable({
-       "language": {
-         "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-         },
-       "processing": true,
-       "serverSide": true, 
-       "ajax":{url:"<?php echo base_url('CGarage/fetch_data'); ?>",
-       type: "POST"
-     },
-       "columnDefs":[
-         {
-             "targets": [1,2,3,4,5,6,7,8],
-         }
-       ]
-     });
-   });
-</script>
-
-<!-- ESTE PARA LAS ALERTAS --->
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<script src="<?php echo base_url();?>assets/js/sweetAlert.js"></script>
-<script type="text/javascript" charset="utf-8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" charset="utf-8" src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
-<script type="text/javascript" charset="utf-8" src="https://cdn.datatables.net/1.10.22/js/dataTables.jqueryui.min.js"></script>
-<script src = "http://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" defer ></script>esheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
-   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.jqueryui.min.css">
-</head>
-<div class="content-wrapper">
-   <div class="content-header">
-      <div class="container-fluid">
-         <div class="row mb-2">
-            <div class="col-sm-6">
-               <h1 class="m-0 text-dark">Garage Vehicular</h1>
-            </div>
-            
-            <!-- /.col -->
-         </div>
-         <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
-      <!-- /.container-fluid -->
-   </div>
-   <section class="content">
-      <div class="box box-info ">
-         <div class="box-body">
-            <div class="table-responsive">
-               <table id="example1" name="example1" class="table table-bordered table-striped" style="width: 100%;">
-                  <thead>
-                     <tr>
-                        <th>Codigo</th>
-                        <!-- 0 ---> 
-                        <th>Patente</th>
-                        <!-- 1 --->
-                        <th>Modelo</th>
-                        <!-- 2 --->
 
                         
                      
@@ -227,7 +144,7 @@
      },
        "columnDefs":[
          {
-             "targets": [1,2],
+             "targets": [1,2,3,4,5,6,7,8],
          }
        ]
      });
