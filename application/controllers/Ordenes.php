1e3d3275eef1587f339ade3ab11b18d7bf59b94f
Rober <ddeltax2@gmail.com>
2021-02-19 18:49:54 -0300

    Avances administracion v29

--- application/controllers/Ordenes.php
+++ application/controllers/Ordenes.php
@@ -16,22 +16,7 @@ class Ordenes extends CI_Controller
     }
 
     public function index(){
-		$set_data = $this->session->all_userdata();
-
-		if (isset($set_data['id_tipousuario']) && $set_data['id_tipousuario'] == 5) {
-		$data['lista_materiales'] = $this->OrdenesModel->listaMateriales();
-        $data['lista_cotizaciones'] = $this->CotizacionesModel->listaCotizaciones();
-        $data['lista_bodegas'] = $this->OrdenesModel->listaBodegas();
-        $data['lista_proyectos'] = $this->Proyecto_model->listaProyectos();
-        $data ['activomenu'] = 15;
-		$data ['activo'] = 15;
-		$this->load->view('menu/menu_proyecto',$data);
-		$this->load->view('Administracion/Proveedores');
-		$this->load->view('layout/footer');
-
-		}else if (isset($set_data['id_tipousuario']) && $set_data['id_tipousuario'] == 1) {
-
-		$data['lista_materiales'] = $this->OrdenesModel->listaMateriales();
+        $data['lista_materiales'] = $this->OrdenesModel->listaMateriales();
         $data['lista_cotizaciones'] = $this->CotizacionesModel->listaCotizaciones();
         $data['lista_bodegas'] = $this->OrdenesModel->listaBodegas();
         $data['lista_proyectos'] = $this->Proyecto_model->listaProyectos();
@@ -41,8 +26,6 @@ class Ordenes extends CI_Controller
      	$this->load->view('menu/menu_supremo',$data);
 		$this->load->view('Administracion/Ordenes',$data);
 		$this->load->view('layout/footer');
-		}
-        
     }
 
     public function listaOrdenes()
@@ -53,21 +36,16 @@ class Ordenes extends CI_Controller
             $sub_array = array();
             $sub_array[] = $value->nroorden;
             $sub_array[] = $value->fecha;
+			$sub_array[] = $value->rut;
 			$sub_array[] = $value->nombre;
 			$sub_array[] = $value->total;
+			
 
-
-			//$sub_array[] = $value->id_cotizacion;
 			if($value->estado == 0){
-				$sub_array[] = '<span class="badge badge-warning">Por aprobar</span>';
+				$sub_array[] = 'Por aprobar';
 			}
 
 			if($value->estado == 1){
-				$sub_array[] = '<span class="badge badge-success">Aprobada</span>';
-			}
-            $sub_array[] = '<button class="btn btn-primary btn-sm" data-toggle="modal" id="eyedetalle-orden" data-target="#modal-detalle-orden" onclick="setTablaDetalle(this)"><i class="far fa-eye"></i></button><button class="btn btn-success btn-sm" data-toggle="modal" id="estado-orden" data-target="#modal-estado-orden" onclick="setTablaEstado(this)"><i class="fas fa-edit"></i></button>';
-            $data[] = $sub_array;
-
 
 				$sub_array[] = '<span class="badge badge-danger">Impagada</span>';
 			}
@@ -89,7 +67,34 @@ class Ordenes extends CI_Controller
         );
         echo json_encode($output);
 
+	}
+	
+
+	public function fetch_vermas_data()
+    {
+        $this->load->model('OrdenesModel', 'reajustar');
+
+		$fetch_data = $this->reajustar->make_model_vermas($this->input->get('id'))[0];
+		$data = array();
+        foreach ($fetch_data as $value){
+            $sub_array = array();
+            $sub_array[] = $value->nroorden;
+            $sub_array[] = $value->nombre;
+			$sub_array[] = $value->cantidad;
+			$sub_array[] = $value->preciounitario;
+			$data[] = $sub_array;
+		}
+			
+
+		$output = array(
+            "draw" => intval($_POST["draw"]) ,
+            "recordsTotal" => $this->reajustar->get_all_data_vermas() ,
+            "recordsFiltered" => $this->reajustar->get_filtered_data_vermas() ,
+            "data" => $data
+        );
+        echo json_encode($data);
     }
+
 
     public function productoNuevo(){
 		if ($this->input->is_ajax_request()) {
@@ -145,26 +150,6 @@ class Ordenes extends CI_Controller
 			echo "'No direct script access allowed'";
 		}
 
-	}
-	
-	public function actualizarEstadoOrden(){
-		if ($this->input->is_ajax_request()) {
-			$ajax_data = $this->input->post();
-			$estado = $ajax_data['estado'];
-			$nroorden = $ajax_data['nroorden'];
-			$res = $this->OrdenesModel->actualizarEstadoOrden($estado,$nroorden); 
-			if ($res) {
-				$data = array('response' => "success", 'message' => "Estado actualizado correctamente!");
-			} 
-			if($res == null){
-				$data = array('response' => "error", 'message' => "Error al actualizar");
-			}
-
-			echo json_encode($data);
-		} else {
-			echo "'No direct script access allowed'";
-		}
-
     }
     
     public function obtenerDetalleOrden(){
@@ -173,7 +158,7 @@ class Ordenes extends CI_Controller
 		$horas_extras = $this->OrdenesModel->ObtenerDetalleOrden($ajax_data['iditem']);
 		
 		$response = "<div class='table-responsive'>";
-		$response .= "<table class='table table-bordered'>";
+		$response .= "<table class='table table-bordered' id='productos_detalle' name='productos_detalle'>";
 		$response .= "<tr>";
 		$response .= "<td>";
 		$response .= "<label>Numero</label>";
@@ -215,18 +200,21 @@ class Ordenes extends CI_Controller
 		$response .= "</table>";
 		$response .= "</div>";
 
+
 		$data = array('response' => 'success', 'detalle' => $response);
 
 
 		echo json_encode($data);
 	}
 
+
 	public function obtenerEstadoOrden(){
 		$ajax_data = $this->input->post(); //Datos que vienen por POST
 		$response = "<select class='form-control select2' style='width: 100%;' id='estado_orden' onchange='setEstadoOrden()'>";
-		$response .= "<option value='0' selected>Seleccione</option>";
-		$response .= "<option value='0'>Por aprobar</option>";
-		$response .= "<option value='1'>Aprobada</option>";
+		$response .= "<option value='1' selected>Seleccione</option>";
+		$response .= "<option value='2'>Pagada</option>";
+		$response .= "<option value='1'>Impagada</option>";
+		$response .= "<option value='0'>Cheque a 30 dias</option>";
 		$response .= "</select>";
 		$data = array('response' => 'success', 'detalle' => $response);
 
@@ -234,6 +222,77 @@ class Ordenes extends CI_Controller
 		echo json_encode($data);
 	}
 
-    
+	public function descargarDetalleOrden(){
+        $ajax_data = $this->input->post();
+        $nro  = $ajax_data['col1'];
+        $material = $ajax_data['col2'];
+        $cantidad = $ajax_data['col3'];
+        $valor = $ajax_data['col4'];
+		$importe = $ajax_data['col5'];
+		$hoy = date("dmyhis");
+        $fechaactual = date("d/m/Y");
+
+        $stylesheet = file_get_contents(base_url().'assets/login/diseño/bootstrap/css/bootstrap.min.css');
+
+        $html = "<div class='row'>";
+		$html .= "<div class='col-md-4'>";
+		$html .= "<div class='form-group'>";
+		$html .= "<img src='".base_url()."assets/Imagen/logooficial.png' class='img-responsive center-block' alt='Cinque Terre'>";
+		$html .= "</div>";
+		$html .= "</div>";
+		$html .= "<div class='col-md-8'>";
+		$html .= "<div class='form-group'>";
+		$html .= "<blockquote class='blockquote text-center'>";
+        $html .= "<p class='mb-0'>CDH Ingenieria</p>";
+        $html .= "<footer class='blockquote-footer'>Orden de compra Nro: (nro)</footer>";
+        $html .= "</blockquote>";
+		$html .= "</div>";
+		$html .= "</div>";
+		$html .= "</div>";
+        $html .= "<br>";
+        $html .= "<div class='table-responsive'>";
+		$html .="<TABLE BORDER class='table table-bordered' id='tabla_vacaciones'>";
+		$html .= "<tr>";
+		$html .= "<td>";
+		$html .= "<label>Cod</label>";
+		$html .= "</td>";
+		$html .= "<td>";
+		$html .= "<label>Material</label>";
+		$html .= "</td>";
+		$html .= "<td>";
+		$html .= "<label>Cantidad</label>";
+		$html .= "</td>";	
+		$html .= "<td>";
+		$html .= "<label>Valor unitario</label>";
+		$html .= "</td>";
+		$html .= "<td>";
+		$html .= "<label>Importe</label>";
+		$html .= "</td>";
+		$html .= "</tr>";
+		$html .= "<tbody>";
+		for($i = 0;$i<count($nro);$i++){ 
+			$html .="<TR>";
+			$html .="<TH>".$nro[$i]."</TH>";
+			$html .="<TH>".$material[$i]."</TH>";
+			$html .="<TH>".$cantidad[$i]."</TH>";
+			$html .="<TH>".$valor[$i]."</TH>";
+			$html .="<TH>".$importe[$i]."</TH>";
+			$html .="</TR>";
+		}
+		$html .= "</tbody>";
+		$html .="</TABLE>";
+        $html .= "</div>";
+
+        //this the the PDF filename that user will get to download
+        $pdfFilePath = "cipdf_".$hoy.".pdf";
+ 
+        //load mPDF library
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
+        $mpdf->WriteHTML($html, \Mpdf\HTMLParserMode::HTML_BODY,true, false);
+		
+        $mpdf->Output($pdfFilePath, "D");
+    }
+
 }
 
