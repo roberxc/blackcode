9ca87010df0756a2a07149d4898fbeb242210884
unknown <dioskratos52@gmail.com>
2021-03-27 13:15:26 -0300

    proyectoEvaluacionguardado03

--- application/views/Proyecto/AdministradorArchivos.php
+++ application/views/Proyecto/AdministradorArchivos.php
@@ -14,42 +14,6 @@
    <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>
 </head>
 <div class="content-wrapper">
-<<<<<<< HEAD
-   <!-- Main Sidebar Container -->
-   <section class="content">
-      <div class="container-fluid">
-         <div class="row">
-            <!-- left column -->
-            <div class="col-md-4">
-               <!-- general form elements -->
-               <div class="card card-primary">
-                  <div class="card-header">
-                     <h3 class="card-title">Carpetas</h3>
-                     <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="card-refresh" data-source="widgets.html" data-source-selector="#card-refresh-content" data-load-on-init="false"><i class="fas fa-sync-alt"></i></button>
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-                     </div>
-                  </div>
-                  <!-- /.card-header -->
-                  <!-- form start -->
-                  <br>
-                  <div class="col-md-12">
-                     <p>
-                        <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#licitaciones" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder"></i> Licitaciones</button>
-                     </p>
-                     <div class="row">
-                        <div class="collapse" id="licitaciones">
-                           <div class="card card-body">
-                              <p>
-                                 <button class="btn btn-secondary"><i class="far fa-file"></i> Antecedentes tecnicos</button>
-                              </p>
-                              <p>
-                                 <button class="btn btn-secondary"><i class="far fa-file"></i> Propuestas tecnicas</button>
-                              </p>
-                              <p>
-                                 <button class="btn btn-secondary"><i class="fas fa-file-image"></i> Fotos</button>
-                              </p>
-=======
    <!-- Content Header (Page header) -->
    <section class="content-header">
       <div class="container-fluid">
@@ -206,104 +170,10 @@
                         <div class="input-group-append">
                            <div class="btn btn-primary">
                               <i class="fas fa-search"></i>
->>>>>>> 4fbd0c22b6bb5cd0b5550f3c69aca8475ba0ce5b
                            </div>
                         </div>
                      </div>
                   </div>
-<<<<<<< HEAD
-                  <!--- COTIZACIONES --->
-                  <div class="col-md-12">
-                     <p>
-                        <button class="btn btn-success" type="button" data-toggle="collapse" data-target="#cotizaciones" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder"></i> Cotizaciones</button>
-                     </p>
-                     <div class="row">
-                        <div class="collapse" id="cotizaciones">
-                           <div class="card card-body">
-                              <!--- Clientes --->
-                              <p>
-                              <button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#clientes" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder"></i> Clientes</button>
-                              </p>
-                              <div class="row">
-                                 <div class="collapse" id="clientes">
-                                    <div class="card card-body">
-                                       <p>
-                                          <button class="btn btn-warning"><i class="fa fa-folder"></i> Cotizaciones</button>
-                                       </p>
-                                       <p>
-                                          <button class="btn btn-warning"><i class="fa fa-folder"></i> Evaluacion de proyectos</button>
-                                       </p>
-                                    </div>
-                                 </div>
-                              </div>
-                             
-                              <!--- Proveedores --->
-                              <p>
-                                 <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#proveedores" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder"></i> Proveedores</button>
-                              </p>
-                              <div class="row">
-                                 <div class="collapse" id="proveedores">
-                                    <div class="card card-body">
-                                       <p>
-                                       <button class="btn btn-danger"><i class="fa fa-folder"></i> Cotizaciones</button>
-                                       </p>
-                                    </div>
-                                 </div>
-                              </div>
-                              <!------------------>
-                              <p>
-                                 <button class="btn btn-success"><i class="fa fa-folder"></i> Documentos tecnicos</button>
-                              </p>
-                              <p>
-                                 <button class="btn btn-success"><i class="fa fa-folder"></i> Planos</button>
-                              </p>
-
-                           </div>
-                           
-                        </div>
-                        
-                     </div>
-                  </div>
-
-               </div>
-            </div>
-            <!--/.col (left) -->
-            <!-- right column -->
-            <div class="col-md-8">
-               <!-- general form elements disabled -->
-               <div class="card card-secondary">
-                  <div class="card-header">
-                     <h3 class="card-title">Archivos</h3>
-                  </div>
-                  <!-- /.card-header -->
-                  <div class="card-body">
-                     <form role="form">
-                        <div class="row">
-                           <div class="col-md-3 col-sm-6 col-12">
-                              <div class="info-box">
-                                 <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
-                                 <div class="info-box-content">
-                                    <span class="info-box-text">Messages</span>
-                                    <span class="info-box-number">1,410</span>
-                                 </div>
-                                 <!-- /.info-box-content -->
-                              </div>
-                              <!-- /.info-box -->
-                           </div>
-                        </div>
-                     </form>
-                  </div>
-                  <!-- /.card-body -->
-               </div>
-               <!-- /.card -->
-            </div>
-            <!--/.col (right) -->
-         </div>
-         <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
-   </section>
-=======
                   <!-- /.card-tools -->
                </div>
                <!-- /.card-header -->
@@ -392,7 +262,6 @@
          </form>
       </div>
    </div>
-
 </div>
 </body>
 <!-- ESTE PARA LAS ALERTAS --->
@@ -409,9 +278,5 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <script>var base_url = '<?php echo base_url();?>';</script>
-
-<script src="<?php echo base_url()?>assets/js/CajaChica/ingreso_cajachica.js"></script>
-
 <script src="<?php echo base_url()?>assets/js/PlanillaProyecto/administrador_archivos.js"></script>
-
 </html>
